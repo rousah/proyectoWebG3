fb60c969d2b2d68f4e787b7fb5ddec9e4575d17e
rousah <emvader@epsg.upv.es>
2018-03-08 18:55:34 +0100

    cambiar redireccion

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,25 +1,25 @@
 const baseURI = 'http://localhost:3000/'
 
-$(document).ready(function() {
-  $('#login-form').submit(function(event) {
-    event.preventDefault();
-    $.ajax({
-      type: "POST",
-      url: baseURI + 'login',
-      data: $(this).serialize(),
-      success: function(data) {
-        console.log(data);
-        if (data.token) {
-          localStorage.setItem('token', data.token);
-          window.location = '/member.html';
-        } else {
-          console.log('No token recieved');
-        }
-      },
-      error: function(error) {
-        console.log(error);
-        $('#fallo-aut').show(200);
-      }
+$(document).ready(function () {
+    $('#login-form').submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: baseURI + 'login',
+            data: $(this).serialize(),
+            success: function (data) {
+                console.log(data);
+                if (data.token) {
+                    localStorage.setItem('token', data.token);
+                    window.location = '/mis-datos.html';
+                } else {
+                    console.log('No token recieved');
+                }
+            },
+            error: function (error) {
+                console.log(error);
+                $('#fallo-aut').show(200);
+            }
+        });
     });
-  });
 });
